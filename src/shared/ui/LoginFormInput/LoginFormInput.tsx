10f0945e66361140cc6af91673fd75bfe6951051
Alexandra Kondratenko <frida21am@mail.ru>
2025-05-12 18:03:35 +0300

    refactor: reduce props

--- src/shared/ui/LoginFormInput/LoginFormInput.tsx
+++ src/shared/ui/LoginFormInput/LoginFormInput.tsx
@@ -7,37 +7,26 @@ interface LoginFormInputProps {
   id: string;
 }
 
-function LoginFormInput({
-  label,
-  type,
-  value,
-  onChange,
-  error,
-  id,
-}: LoginFormInputProps) {
+function LoginFormInput(props: LoginFormInputProps) {
   return (
     <div className="my-4">
-      <label htmlFor={id} className="text-lg">
-        {label}
+      <label htmlFor={props.id} className="text-lg">
+        {props.label}
       </label>
       <input
-        type={type}
-        name={id}
-        id={id}
         className="mt-1 p-5 block w-full border-2 border-solid border-gray-200 rounded-md focus:outline-none focus:ring focus:ring-blue-400"
-        value={value}
-        onChange={onChange}
-        aria-invalid={error ? "true" : "false"}
-        aria-describedby={error ? `${id}-error` : undefined}
+        aria-invalid={props.error ? "true" : "false"}
+        aria-describedby={props.error ? `${props.id}-error` : undefined}
         required
+        {...props}
       />
-      {error && (
+      {props.error && (
         <span
-          id={`${id}-error`}
+          id={`${props.id}-error`}
           role="alert"
           className="text-red-600 inline-block mt-1"
         >
-          {error}
+          {props.error}
         </span>
       )}
     </div>
