f3a0fd3c32aad7d83b758de49f19c8a9dd8ba2b7
Alexandra Kondratenko <frida21am@mail.ru>
2025-05-23 18:23:43 +0300

    perf: add react lazy

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,45 @@ import "./app/styles/index.css";
 import Layout from "./app/layouts/Layout";
 import GuestRoute from "./app/routes/GuestRoute";
 import PrivateRoute from "./app/routes/PrivateRoute";
-import HomePage from "./pages/home/ui/HomePage";
-import LoginPage from "./pages/login/ui/LoginPage";
-import NotFoundPage from "./pages/not-found/ui/NotFoundPage";
-import ProfilePage from "./pages/profile/ui/ProfilePage";
 import { AuthProvider } from "./app/context/AuthProvider";
+import React, { Suspense } from "react";
+
+const HomePage = React.lazy(() => import("./pages/home/ui/HomePage"));
+const LoginPage = React.lazy(() => import("./pages/login/ui/LoginPage"));
+const NotFoundPage = React.lazy(
+  () => import("./pages/not-found/ui/NotFoundPage")
+);
+const ProfilePage = React.lazy(() => import("./pages/profile/ui/ProfilePage"));
 
 function App() {
   return (
     <>
       <AuthProvider>
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<HomePage />} />
-              <Route
-                path="login"
-                element={
-                  <GuestRoute>
-                    <LoginPage />
-                  </GuestRoute>
-                }
-              />
-              <Route
-                path="profile"
-                element={
-                  <PrivateRoute>
-                    <ProfilePage />
-                  </PrivateRoute>
-                }
-              />
-              <Route path="*" element={<NotFoundPage />} />
-            </Route>
-          </Routes>
+          <Suspense fallback={<div>Загрузка...</div>}>
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route index element={<HomePage />} />
+                <Route
+                  path="login"
+                  element={
+                    <GuestRoute>
+                      <LoginPage />
+                    </GuestRoute>
+                  }
+                />
+                <Route
+                  path="profile"
+                  element={
+                    <PrivateRoute>
+                      <ProfilePage />
+                    </PrivateRoute>
+                  }
+                />
+                <Route path="*" element={<NotFoundPage />} />
+              </Route>
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </AuthProvider>
     </>
